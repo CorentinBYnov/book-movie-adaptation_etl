9aeb9f2752ade7c6d1af096868ec625f3ffd5342
CorentinBYnov <corentin.bouchard@ynov.com>
2026-06-26 11:12:24 +0200

    feat: add join table

--- scripts/load/load_data.py
+++ scripts/load/load_data.py
@@ -5,6 +5,13 @@ import pandas as pd
 df_books = pd.read_csv("data/intermediaire/books_clean.csv")
 df_movies = pd.read_csv("data/intermediaire/movies_clean.csv")
 
+df_books_subset = df_books[["title", "id"]].rename(columns={"id": "book_id"})
+df_movies_subset = df_movies[["title", "rank"]].rename(columns={"rank": "movie_id"})
+
+df_matching_ids = df_books_subset.merge(df_movies_subset, on="title", how="inner")
+
+df_book_movie_adaptations = df_matching_ids.drop(columns=["title"])
+
 
 def main():
     con = sqlite3.connect("database/cinema.db")
@@ -38,8 +45,21 @@ def main():
         );
     """)
 
+    cur.execute("""
+        CREATE TABLE book_movie_adaptations (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            book_id INTEGER NOT NULL,
+            movie_id INTEGER NOT NULL,
+            FOREIGN KEY (book_id) REFERENCES books(id),
+            FOREIGN KEY (movie_id) REFERENCES movies(rank)
+        );
+    """)
+
     df_books.to_sql("books", con, if_exists="append", index=False)
     df_movies.to_sql("movies", con, if_exists="append", index=False)
+    df_book_movie_adaptations.to_sql(
+        "book_movie_adaptations", con, if_exists="append", index=False
+    )
 
     con.close()
 
